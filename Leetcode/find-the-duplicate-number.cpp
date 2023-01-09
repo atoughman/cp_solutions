c2cfac7b89a1346eb9ea69ed42bc24104c39b537
atoughman <amanofficial950@gmail.com>
2023-01-09 15:33:16 +0530

    9/1/23

---
+++ Leetcode/find-the-duplicate-number.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        // Floyd's Hare and Tortoise Algorithm
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
