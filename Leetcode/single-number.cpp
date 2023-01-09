7dfedf8647bc60286b04e49d036f3871246bfd5d
atoughman <amanofficial950@gmail.com>
2023-01-09 22:49:26 +0530

    9/1/23

---
+++ Leetcode/single-number.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int xorr = 0;
+        for (auto &x : nums)
+            xorr ^= x;
+
+        return xorr;
+    }
+};
