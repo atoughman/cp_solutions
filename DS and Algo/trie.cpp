fdccc0ee11735ac2b53091e94f2b2eb682d7645d
atoughman <amanofficial950@gmail.com>
2023-02-08 11:38:57 +0530

    8/2/23 : Trie

---
+++ DS and Algo/trie.cpp
@@ -0,0 +1,52 @@
+// problem link
+// https://leetcode.com/problems/implement-trie-prefix-tree/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node {
+    char ch;
+    map<char, node*> mp;
+    bool is_last;
+
+    node(char x) {
+        ch = x;
+        is_last = false;
+    }
+};
+
+class Trie {
+public:
+    node* root;
+    Trie() {
+        root = new node('#'); // doesn't matter as it is dummy
+    }
+    
+    void insert(string word) {
+        node* cur = root;
+        for(auto x: word) {
+            if(!cur->mp[x]) cur->mp[x] = new node(x);
+            cur = cur->mp[x];
+        }
+        cur->is_last = true;
+    }
+
+    node* match(string a) {
+        node* cur = root;
+        for(auto &x: a) {
+            if(!cur->mp[x]) return NULL;
+            cur = cur->mp[x];
+        }
+        return cur;
+    }
+    
+    bool search(string word) {
+        node* cur = match(word);
+        return cur == NULL ? false : cur->is_last;
+    }
+    
+    bool startsWith(string prefix) {
+        node* cur = match(prefix);
+        return cur == NULL ? false : true;
+    }
+};
