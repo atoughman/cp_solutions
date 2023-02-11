9dd9bd75dc9eb4b252e980b1a18c915659f4a88b
atoughman <amanofficial950@gmail.com>
2023-02-11 11:41:34 +0530

    11/2/23 ( grid ) : double binary search ( guess and count )

--- Leetcode/kth-smallest-element-in-a-sorted-matrix.cpp
+++ Leetcode/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,35 +1,32 @@
+// problem link
+// https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int kthSmallest(vector<vector<int>> &matrix, int k)
-    {
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int l = matrix[0][0];
         int n = matrix.size();
-        int lv = matrix[0][0];
-        int rv = matrix[n - 1][n - 1];
+        int r = matrix[n-1][n-1];
 
-        while (lv < rv)
-        {
-            int mv = lv + (rv - lv) / 2;
+        // let's guess ( will take O(log n) time)
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+
+            // find how many elements are smaller or equal than our guess.
+            // n * log(n)
             int ct = 0;
-            for (int i = 0; i < n; i++)
-            {
-                auto it = upper_bound(matrix[i].begin(), matrix[i].end(), mv);
-                int dis = it - matrix[i].begin();
+            for(int i=0; i<n; i++) {
+                int dis = upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+                if(dis == 0) break;
                 ct += dis;
             }
-            if (ct < k)
-                lv = mv + 1;
-            else
-                rv = mv;
-            // cannot return mv for ct == k, bcoz mv might not exist in matrix
-            // we are playing guessing game, where mv is just guessed value.
-            // with this approach, we are narrowing down to a value which exist in matrix
-            // and satisfy the condition.
+            if(ct < k) l = mid + 1;
+            else r = mid - 1;
         }
 
-        return lv;
+        return l;
     }
 };
