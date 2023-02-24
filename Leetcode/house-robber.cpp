0c46c1e04b2f046002745f6e2d7c789adde4d808
atoughman <amanofficial950@gmail.com>
2023-02-24 22:07:02 +0530

    24/2/23 (add) : other approaches

--- Leetcode/house-robber.cpp
+++ Leetcode/house-robber.cpp
@@ -4,6 +4,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// ------------------- recursion + memo
+
+const int mxn = 100 + 5;
+class Solution {
+public:
+    int dp[mxn];
+
+    int solve(int i, vector<int>& nums) {
+        if(i<0) return 0;
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = max(solve(i-2, nums) + nums[i], solve(i-1, nums));
+    }
+
+    int rob(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return solve(nums.size()-1, nums);
+    }
+};
+// ======================================================
+
+// ------------------- Iterative + memo
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        
+        int dp[n];
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], dp[0]);
+
+        for(int i=2; i<n; i++) {
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1]);
+        }
+
+        return dp[n - 1];
+    }
+};
+// ======================================================
+
+// ------------------- Iterative + vars [ BEST ]
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -23,3 +66,4 @@ public:
         
     }
 };
+// ======================================================
