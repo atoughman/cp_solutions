d898be293316a1dbba976ac847332f76dcba09c0
atoughman <amanofficial950@gmail.com>
2023-02-27 12:41:04 +0530

    27/2/23 ( DP ) : min sum to target

---
+++ Leetcode/perfect-squares.cpp
@@ -0,0 +1,21 @@
+// problem link
+// https://leetcode.com/problems/perfect-squares/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSquares(int n) {
+        int dp[n+1];
+
+        for(int i=0; i<=n; i++) {
+            dp[i] = i;
+            for(int j=1; j*j<=i; j++) {
+                dp[i] = min(dp[i], dp[i-j*j]+1);
+            }
+        }
+
+        return dp[n];
+    }
+};
