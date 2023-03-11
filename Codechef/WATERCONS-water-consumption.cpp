3a9e254536683d9754275adfeef922ddf6c4688a
atoughman <amanofficial950@gmail.com>
2023-03-11 06:25:34 +0530

    11/3/23 ( daily ) : practice

---
+++ Codechef/WATERCONS-water-consumption.cpp
@@ -0,0 +1,21 @@
+// problem link
+// https://www.codechef.com/problems/WATERCONS
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Answer() {
+    int x;
+    cin >> x;
+    if(x >= 2000) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    int t = 1;
+    cin >> t;
+    
+    while(t--) Answer();
+    
+    return 0;
+}
