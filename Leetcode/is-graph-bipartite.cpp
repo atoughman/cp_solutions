0ba391a3769a5bc09cb873c501b2c8b41dbb930c
atoughman <amanofficial950@gmail.com>
2023-03-11 20:30:58 +0530

    optimize ( graph ) : enhance readability + array instead of vector

--- Leetcode/is-graph-bipartite.cpp
+++ Leetcode/is-graph-bipartite.cpp
@@ -1,36 +1,35 @@
 // problem link
 // https://leetcode.com/problems/is-graph-bipartite/description/
 
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    bool dfs(int u, vector<vector<int>>& graph, int col, vector<int>& vis) {
+    bool dfs(int u, vector<vector<int>>& graph, int col, int vis[]) {
         vis[u] = col;
-        bool ans = true;
         for(auto v: graph[u]) {
             if(vis[v] == -1) {
-                ans &= dfs(v, graph, !col, vis);
-                if(!ans) return false;
+                if(dfs(v, graph, !col, vis) == false) return false;
             } else {
-                if(vis[v] == col) {
-                    return false;
-                }
+                if(vis[v] == col) return false;
             }
         }
 
-        return ans;
+        return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
         int nodes = graph.size();
-        vector<int> vis(nodes+1, -1);
-        bool ans = true;
+        int vis[nodes+1];
+        memset(vis, -1, sizeof(vis));
+
         for(int i=0; i<nodes; i++) {
-            if(vis[i] == -1) {
-                ans &= dfs(i,graph, 0, vis);
-                if(ans == false) break;
-            }
+            if(vis[i] != -1) continue;
+
+            if(dfs(i, graph, 0, vis) == false) return false;
         }
 
-        return ans;
+        return true;
     }
 };
