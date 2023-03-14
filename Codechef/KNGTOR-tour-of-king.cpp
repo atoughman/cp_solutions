5d4e0ceefe6fef51c54aee52e2cd6b7565ac123c
atoughman <amanofficial950@gmail.com>
2023-03-14 06:53:10 +0530

    14/3/23 ( daily ) : practice

---
+++ Codechef/KNGTOR-tour-of-king.cpp
@@ -0,0 +1,20 @@
+// problem link
+// https://www.codechef.com/problems/KNGTOR
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Answer() {
+    int n,m;
+    cin >> n >> m;
+    cout << (n*5 + m*7) << endl;
+}
+
+int main() {
+    int t = 1;
+    cin >> t;
+    
+    while(t--) Answer();
+    
+    return 0;
+}
