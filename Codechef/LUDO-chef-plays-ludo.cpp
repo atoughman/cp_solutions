2506537066589fc1d51d9d271baed6cf7ecae105
atoughman <amanofficial950@gmail.com>
2023-03-14 06:53:14 +0530

    14/3/23 ( daily ) : practice

---
+++ Codechef/LUDO-chef-plays-ludo.cpp
@@ -0,0 +1,20 @@
+// problem link
+// https://www.codechef.com/problems/LUDO
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Answer() {
+    int n;
+    cin >> n;
+    cout << (n == 6 ? "YES" : "NO") << endl;
+}
+
+int main() {
+    int t = 1;
+    cin >> t;
+    
+    while(t--) Answer();
+    
+    return 0;
+}
