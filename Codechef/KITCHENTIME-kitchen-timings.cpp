8299d7d5a723d4dbdbd0a31851924ca61850ac79
atoughman <amanofficial950@gmail.com>
2023-03-15 07:06:38 +0530

    15/3/23 ( daily ) : practice

---
+++ Codechef/KITCHENTIME-kitchen-timings.cpp
@@ -0,0 +1,20 @@
+// problem link
+// https://www.codechef.com/problems/KITCHENTIME
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Answer() {
+    int x,y;
+    cin >> x >> y;
+    cout << y-x << endl;
+}
+
+int main() {
+    int t = 1;
+    cin >> t;
+    
+    while(t--) Answer();
+    
+    return 0;
+}
