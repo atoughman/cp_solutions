8065e8b16eda4c2b632ec917a4fa89e3e8082bff
atoughman <amanofficial950@gmail.com>
2023-03-15 07:06:35 +0530

    15/3/23 ( daily ) : practice

---
+++ Codechef/FIT-fitness.cpp
@@ -0,0 +1,20 @@
+// problem link
+// https://www.codechef.com/problems/FIT
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Answer() {
+    int x;
+    cin >> x;
+    cout << x*10 << endl;
+}
+
+int main() {
+    int t = 1;
+    cin >> t;
+    
+    while(t--) Answer();
+    
+    return 0;
+}
