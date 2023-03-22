f194b8a1a5e764b65f6ebb2ea976660095565b6c
atoughman <amanofficial950@gmail.com>
2023-03-22 15:21:45 +0530

    update(script) : to dynamically create/update badges

--- problemSolvedCounter.js
+++ problemSolvedCounter.js
@@ -1,7 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 
-const ignoreList = [];
+const ignoreList = ["DS and Algo"];
 
 const countFiles = async (dir, ignoreList = []) => {
     let count = 0;
@@ -30,47 +30,74 @@ const countFiles = async (dir, ignoreList = []) => {
 const countFilesInSubFolder = async (dir, ignoreList = []) => {
     let count = 0;
     const entries = await fs.readdir(dir);
-    const files = entries.filter(
-        (entry) => {
-            if(ignoreList.includes(entry)) {
-                console.log("Ignoring ", entry);
-            }
-            if(entry.startsWith(".")) {
-                console.log("Ignoring ", entry);
-            }
-            return !ignoreList.includes(entry) && !entry.startsWith(".")
+    const files = entries.filter((entry) => {
+        if (ignoreList.includes(entry)) {
+            console.log("Ignoring ", entry);
         }
-    );
+        if (entry.startsWith(".")) {
+            console.log("Ignoring ", entry);
+        }
+        return !ignoreList.includes(entry) && !entry.startsWith(".");
+    });
 
     // count only files in folders of current directory...
+    const filesCount = {};
     for (const entry of files) {
         const entryPath = path.join(dir, entry);
         const stat = await fs.stat(entryPath);
 
         if (stat.isDirectory()) {
-            count += await countFiles(entryPath, ignoreList);
-        }else {
+            const res = await countFiles(entryPath, ignoreList);
+            filesCount[entry] = res;
+            count += res;
+        } else {
             console.log("Ignoring ", entry);
         }
     }
-
-    return count;
+    console.log(filesCount);
+    return filesCount;
 };
 
 async function main() {
     try {
-        const totalFiles = await countFilesInSubFolder("./", ignoreList);
-        console.log(`Total number of files: ${totalFiles}`);
+        const filesCount = await countFilesInSubFolder("./", ignoreList);
+        const totalFiles = Object.values(filesCount).reduce((a, b) => a + b, 0);
+        console.log(`\nTotal number of files: ${totalFiles}\n`);
+        
+        const subfolderList = Object.entries(filesCount);
 
-        // update the readme.md file with the total number of problems solved
+        // Sort subfolders by count of files
+        subfolderList.sort((a, b) => b[1] - a[1]);
+
+        // Generate badges for each subfolder
+        const badges = subfolderList.map(
+            (subfolder) =>
+                `   <img src="https://img.shields.io/badge/${subfolder[0]}-${subfolder[1]}-brightgreen" alt="${subfolder[0]}" width="auto" height="30px">&nbsp;&nbsp;&nbsp;`
+        );
+
+        // Update the readme.md file with the badges
         const readmePath = path.join(__dirname, "readme.md");
         const readmeContent = await fs.readFile(readmePath, "utf-8");
-        const newReadmeContent = readmeContent.replace(
+
+        const startMarker = "<!-- Shields -->";
+        const endMarker = "<!-- About -->";
+        const startIndex = readmeContent.indexOf(startMarker);
+        const endIndex = readmeContent.indexOf(endMarker);
+
+        let newReadmeContent = readmeContent.replace(
             /Total Problems solved = \d+/,
             `Total Problems solved = ${totalFiles}`
         );
+
+        newReadmeContent =
+            newReadmeContent.slice(0, startIndex + startMarker.length) +
+            `\n<div class="shields" style="display:flex">\n` +
+            badges.join("\n") +
+            `\n</div>\n` +
+            newReadmeContent.slice(endIndex);
+
         await fs.writeFile(readmePath, newReadmeContent);
-        console.log("Updated the readme.md file successfully.");
+        console.log("Updated the readme.md file successfully.\n");
     } catch (error) {
         console.error(error);
     }
